ba9b48bc2d0c66852911163982405d87ca3c7bfd
Daniel Vo <dvo@midwayusa.com>
2016-02-13 14:29:59 -0600

    Setup web project for development

---
+++ MarkdownMagic/MarkdownMagic.App/Hubs/MainHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace MarkdownMagic.App.Hubs
+{
+    public class MainHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ MarkdownMagic/MarkdownMagic.App/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MarkdownMagic.App.Startup))]
+
+namespace MarkdownMagic.App
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
